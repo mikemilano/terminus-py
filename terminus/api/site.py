42eb1cbbda4d7b98a940712a2187f9bf4614279e
Mike Milano <coder1@gmail.com>
2013-10-31 04:28:08 -0700

    api site refactoring

--- terminus/api/site.py
+++ terminus/api/site.py
@@ -1,178 +1,177 @@
-import terminus
+import api
 
 
-class Site:
-    def __init__(self, session, uuid):
-        self.session = session
-        self.uuid = uuid
+def info(session, uuid):
+    payload = {'site': uuid, 'path': ''}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-        # Default environments
-        self.environments = {
-            'dev': self.default_environment(),
-            'test': self.default_environment(),
-            'live': self.default_environment()
-        }
 
-        # API vars
-        self.utm_campaign = None
-        self.service_level = None
-        self.name = None
-        self.creator = None
-        self.created_at = None
-        self.created = 0
-        self.utm_source = None
-        self.instrument = None
-        self.upstream = {'url': None, 'branch': None},
-        self.owner = None
-        self.organization = None
-        self.preferred_zone = None
+def state(session, uuid):
+    payload = {'site': uuid, 'path': 'state'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def default_environment(self):
-        return {
-            'diffstat': {},
-            'environment_created': 0,
-            'on_server_development': False,
-            'lock': {'username': None, 'password': None, 'locked': False}
-        }
 
-    # API Calls
-    def info(self):
-        payload = {'site': self.uuid, 'path': ''}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def delete(session, uuid):
+    return None
 
-    def state(self):
-        payload = {'site': self.uuid, 'path': 'state'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def delete(self):
-        return None
+def settings(session, uuid):
+    payload = {'site': uuid, 'path': 'settings'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def settings(self):
-        payload = {'site': self.uuid, 'path': 'settings'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def get_service_level(self):
-        payload = {'site': self.uuid, 'path': 'service-level'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def get_service_level(session, uuid):
+    payload = {'site': uuid, 'path': 'service-level'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def set_service_level(self, service_level):
-        return None
 
-    def get_attribute(self, attribute):
-        payload = {'site': self.uuid, 'path': 'attributes/'+attribute}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def set_service_level(session, uuid, service_level):
+    return None
 
-    def set_attribute(self, attribute, value):
-        return None
 
-    def get_attributes(self):
-        payload = {'site': self.uuid, 'path': 'attributes'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def get_attribute(session, uuid, attribute):
+    payload = {'site': uuid, 'path': 'attributes/'+attribute}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def set_attributes(self, attributes):
-        return None
 
-    def delete_attributes(self):
-        return None
+def set_attribute(session, uuid, attribute, value):
+    return None
 
-    def get_upstream(self):
-        payload = {'site': self.uuid, 'path': 'code-upstream'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def get_team_members(self):
-        payload = {'site': self.uuid, 'path': 'team'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def get_attributes(session, uuid):
+    payload = {'site': uuid, 'path': 'attributes'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def delete_team_member(self, uuid):
-        return None
 
-    def invite_team_member(self, email, invited_by):
-        return None
+def set_attributes(session, uuid, attributes):
+    return None
 
-    def verify_team_member(self, uid):
-        return None
 
-    def get_owner(self):
-        payload = {'site': self.uuid, 'path': 'owner'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def delete_attributes(session, uuid):
+    return None
 
-    def screenshot(self):
-        return None
 
-    def events(self, environment, limit=100):
-        payload = {'site': self.uuid, 'path': 'environments/'+environment+'?limit='+limit}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def get_upstream(session, uuid):
+    payload = {'site': uuid, 'path': 'code-upstream'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def get_binding(self, environment, type):
-        payload = {'site': self.uuid, 'path': 'environment/'+environment+'/bindings?type='+type}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def get_bindings(self, environment):
-        payload = {'site': self.uuid, 'path': 'environment/'+environment+'/bindings'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def get_team_members(session, uuid):
+    payload = {'site': uuid, 'path': 'team'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def clear_cache(self, environment):
-        return None
 
-    def get_onserver_status(self, environment):
-        payload = {'site': self.uuid, 'path': 'environments/'+environment+'/on-server-development'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def delete_team_member(session, uuid):
+    return None
 
-    def set_onserver_status(self, environment, status):
-        return None
 
-    def commit(self, environment, message, uuid):
-        return None
+def invite_team_member(session, uuid, email, invited_by):
+    return None
 
-    def diff(self, environment, filepath):
-        payload = {'site': self.uuid, 'path': 'environments/'+environment+'/on-server-development/diffstat'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def diff_stats(self, environment):
-        return None
+def verify_team_member(session, uuid, uid):
+    return None
 
-    def lock(self, environment, username, password):
-        return None
 
-    def delete_lock(self, environment):
-        return None
+def get_owner(session, uuid):
+    payload = {'site': uuid, 'path': 'owner'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def hostnames(self, environment):
-        payload = {'site': self.uuid, 'path': 'environments/'+environment+'/hostnames'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def add_hostname(self, environment, hostname):
-        return None
+def screenshot(session, uuid):
+    return None
 
-    def delete_hostname(self, environment, hostname):
-        return None
 
-    def environments(self):
-        payload = {'site': self.uuid, 'path': 'environments'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def events(session, uuid, environment, limit=100):
+    payload = {'site': uuid, 'path': 'environments/'+environment+'?limit='+limit}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
 
-    def notifications(self):
-        payload = {'site': self.uuid, 'path': 'notifications'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
 
-    def bindings(self):
-        payload = {'site': self.uuid, 'path': 'bindings'}
-        response = self.session.get(terminus.TURL, params=payload)
-        return terminus.handle_response(response)
+def get_binding(session, uuid, environment, type):
+    payload = {'site': uuid, 'path': 'environment/'+environment+'/bindings?type='+type}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def get_bindings(session, uuid, environment):
+    payload = {'site': uuid, 'path': 'environment/'+environment+'/bindings'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def clear_cache(session, uuid, environment):
+    return None
+
+
+def get_onserver_status(session, uuid, environment):
+    payload = {'site': uuid, 'path': 'environments/'+environment+'/on-server-development'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def set_onserver_status(session, uuid, environment, status):
+    return None
+
+
+def commit(session, uuid, environment, message):
+    return None
+
+
+def diff(session, uuid, environment, filepath):
+    payload = {'site': uuid, 'path': 'environments/'+environment+'/on-server-development/diffstat'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def diff_stats(session, uuid, environment):
+    return None
+
+
+def lock(session, uuid, environment, username, password):
+    return None
+
+
+def delete_lock(session, uuid, environment):
+    return None
+
+
+def hostnames(session, uuid, environment):
+    payload = {'site': uuid, 'path': 'environments/'+environment+'/hostnames'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def add_hostname(session, uuid, environment, hostname):
+    return None
+
+
+def delete_hostname(session, uuid, environment, hostname):
+    return None
+
+
+def environments(session, uuid):
+    payload = {'site': uuid, 'path': 'environments'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def notifications(session, uuid):
+    payload = {'site': uuid, 'path': 'notifications'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
+
+
+def bindings(session, uuid):
+    payload = {'site': uuid, 'path': 'bindings'}
+    response = session.get(api.TURL, params=payload)
+    return api.handle_response(response)
